8fd23d1e85d62af17631f62588a684fee90ed636
Bahdanovich <bahdanovich@internet.ru>
2023-10-06 22:55:59 +0300

    Added ChatController

---
+++ app/Http/Controllers/API/ChatController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Chat;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class ChatController extends Controller
+{
+    public function index()
+    {
+        return Chat::all();
+    }
+    public function show(Chat $chat): Chat
+    {
+        return $chat;
+    }
+    public function store(Request $request): JsonResponse
+    {
+        $chat = Chat::create($request->all());
+
+        return response()->json($chat, 201);
+    }
+    public function update(Request $request, Chat $chat): JsonResponse
+    {
+        $chat->update($request->all());
+
+        return response()->json($chat, 200);
+    }
+    public function delete(Chat $chat): JsonResponse
+    {
+        $chat->delete();
+
+        return response()->json(null, 204);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,15 @@
 <?php
 
+use App\Http\Controllers\API\ChatController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('chats', [ChatController::class, 'index']);
+Route::get('chats/{chat}', [ChatController::class, 'show']);
+Route::post('chats', [ChatController::class, 'store']);
+Route::put('chats/{chat}', [ChatController::class, 'update']);
+Route::delete('chats/{chat}', [ChatController::class, 'delete']);
